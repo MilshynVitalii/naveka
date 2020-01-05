b4a0b98bcb5260e3ed459502a7a737899a22a95e
MilshynVitalii <milshyn.vitalii@gmail.com>
2020-01-05 19:18:45 +0200

    carousel

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$('.projects-carousel').slick({
+		dots: true,
+		autoplay: false,
+		autoplaySpeed: 4500,
+		speed: 1500,
+		infinite: true,
+		slidesToShow: 3,
+ 		slidesToScroll: 3,
+ 		arrows: true,
+	});
+});
