5ea83e04f39cc15aedb86a644be19d7597f2fc61
MilshynVitalii <milshyn.vitalii@gmail.com>
2020-01-07 20:30:19 +0200

    homes-section

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 	$('.projects-carousel').slick({
 		dots: true,
 		autoplay: false,
-		autoplaySpeed: 4500,
-		speed: 1500,
+		speed: 1000,
 		infinite: true,
 		slidesToShow: 3,
  		slidesToScroll: 3,
